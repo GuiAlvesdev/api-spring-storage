21e76d06681fe439c20a1db7f5c4fcb07df8e283
GuiAlvesdev <guialves1353@gmail.com>
2022-11-05 20:42:43 -0300

    first commit :tada:

---
+++ back-end/src/main/java/com/guialvesdev/springfoodapi/SpringFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.guialvesdev.springfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFoodApiApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/guialvesdev/springfoodapi/SpringFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guialvesdev.springfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
