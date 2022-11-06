4730aa5a8e68c34ebe1998bc94eb36662b1e5988
GuiAlvesdev <guialves1353@gmail.com>
2022-11-06 00:20:41 -0300

    add relations database :rocket:

---
+++ back-end/src/main/java/com/guialvesdev/springfoodapi/repository/CozinhaRepository.java
@@ -0,0 +1,22 @@
+package com.guialvesdev.springfoodapi.repository;
+
+import com.guialvesdev.springfoodapi.domain.Cozinha;
+
+public class CozinhaRepository {
+
+    List<Cozinha> listar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
