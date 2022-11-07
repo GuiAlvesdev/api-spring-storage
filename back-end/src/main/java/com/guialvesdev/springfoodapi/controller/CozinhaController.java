786ee303ba9bd5f29ac83c3d67d1226701a2d6b3
GuiAlvesdev <guialves1353@gmail.com>
2022-11-06 23:04:18 -0300

    add coding http :work:

--- back-end/src/main/java/com/guialvesdev/springfoodapi/controller/CozinhaController.java
+++ back-end/src/main/java/com/guialvesdev/springfoodapi/controller/CozinhaController.java
@@ -2,12 +2,12 @@ package com.guialvesdev.springfoodapi.controller;
 
 import com.guialvesdev.springfoodapi.domain.Cozinha;
 import com.guialvesdev.springfoodapi.repository.CozinhaRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +28,19 @@ public class CozinhaController {
     public List<Cozinha> listar(){
         return cozinhaRepository.listar();
 
+
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId){
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+       if (cozinha != null){
+           return ResponseEntity.ok(cozinha);
+
+       }
+       return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+       return ResponseEntity.notFound().build();
 
     }
 
