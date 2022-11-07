8a42e0d7a7f458bd2dc53baccc54ca1ad503425e
GuiAlvesdev <guialves1353@gmail.com>
2022-11-07 17:16:56 -0300

    implements criteria api :work:

--- back-end/src/main/java/com/guialvesdev/springfoodapi/insfrastruture/repository/RestauranteRepositoryImpl.java
+++ back-end/src/main/java/com/guialvesdev/springfoodapi/insfrastruture/repository/RestauranteRepositoryImpl.java
@@ -1,11 +1,14 @@
 package com.guialvesdev.springfoodapi.insfrastruture.repository;
 
 import com.guialvesdev.springfoodapi.domain.model.Restaurante;
+import org.hibernate.criterion.CriteriaQuery;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -18,29 +21,13 @@ public class RestauranteRepositoryImpl implements  RestauranteRepositoryQueries{
     @Override
     public List<Restaurante> find(String nome,
              BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
 
-        var jpql = new StringBuilder();
-        jpql.append("from Restaurante where 0 = 0 ");
+        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+        criteria.from(Restaurante.class);
 
-        if (StringUtils.hasLength(nome)){
-            jpql.append("where nome like :nome ");
-        }
-
-        if(taxaFreteInicial != null){
-            jpql.append("and taxaFrete >= :taxaInicial ");
-        }
-
-        if(taxaFreteInicial != null){
-            jpql.append("and taxaFrete <= :taxaFinal ");
-        }
-
-
-        return manager .createQuery(jpql.toString(), Restaurante.class)
-                .setParameter("nome", "%" + "%")
-                .setParameter("taxaInicial", taxaFreteInicial)
-                .setParameter("taxaFinal", taxaFreteFinal)
-                .getResultList();
-
+        TypedQuery<Restaurante> query = manager.createQuery(criteria);
+        return query.getResultList();
 
 
 
